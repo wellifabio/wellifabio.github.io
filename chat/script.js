3629f19526e6a6e1babd44a05353ae63de23175a
wellifabio <wellifabio@hotmail.com>
2022-10-28 14:06:44 -0300

    chat

--- chat/script.js
+++ chat/script.js
@@ -11,6 +11,14 @@ const cores = {
     4: "preto"
 }
 
+const emogis = {
+    0: "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/dog-face_1f436.png",
+    1: "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/cat-face_1f431.png",
+    2: "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/mouse-face_1f42d.png",
+    3: "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/panda_1f43c.png",
+    4: "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/orangutan_1f9a7.png"
+}
+
 function enviar() {
     let msg = {
         nick: nick,
@@ -28,12 +36,15 @@ socket.on("resp", (data) => {
 const baloes = (msg) => {
     let balao = document.createElement("div");
     let quem = document.createElement("label");
+    let emogi = document.createElement("img");
     let oque = document.createElement("div");
-    quem.innerHTML = msg.nick;
+    emogi.setAttribute("src", emogis[msg.cor]);
+    quem.innerHTML += msg.nick;
     oque.innerHTML = msg.msg;
     oque.classList.add(cores[msg.cor]);
     balao.classList.add('balao');
     balao.appendChild(quem);
+    balao.appendChild(emogi);
     balao.appendChild(oque);
     recebe.appendChild(balao);
 }
@@ -42,4 +53,5 @@ const nickname = () => {
     nick = prompt("Digite um nickname/apelido");
     cor = parseInt(Math.random() * 5);
     document.querySelector("#nick").innerHTML = nick;
+    document.querySelector("#emogi").src=emogis[cor];
 }
