0f10a152dc1283a9345ca30325036bf1b2dbdd57
wellifabio <wellifabio@hotmail.com>
2023-03-27 22:00:37 -0300

    timeline

--- timeline/script.js
+++ timeline/script.js
@@ -3,7 +3,16 @@ const header = body.childNodes[1];
 const main = body.childNodes[3];
 const footer = body.childNodes[5];
 const modal = body.childNodes[7];
+const add = document.querySelector("#add");
+
+const cores = {
+    "Planejado" : "rgb(24, 154, 225);",
+    "Atrasado" : "rgb(238, 190, 0);",
+    "Execução" : "rgb(202, 52, 32);",
+    "Concluído" : "rgb(8, 75, 33);",
+}
 var dados = [];
+
 
 const carregaArquivo = (json) => {
     fetch(json)
@@ -13,15 +22,26 @@ const carregaArquivo = (json) => {
                 dados = resp;
                 cabecalho();
                 principal();
+                rodape();
             }
         )
 };
 
+add.addEventListener("submit", (e) => {
+    e.preventDefault();
+    modal.classList.toggle("oculto");
+})
+
 const cabecalho = () => {
-    header.innerHTML = "Projeto " + dados[0].projeto
+    header.innerHTML = "<h1>Projeto " + dados[0].projeto + "</h1>";
 }
 
-const principal = () => {
+const rodape = () => {
+    footer.innerHTML = "<h3>Início do projéto em: </h3> <input type='date' value='" + dados[0].inicio + "'/>";
+    footer.innerHTML += `<button onclick='modal.classList.toggle("oculto");'>Adicionar tarefa</button>`;
+}
+
+const principal = async () => {
     let tabela = document.createElement("table")
     tabela.appendChild(thead())
     tabela.appendChild(tbody())
@@ -31,10 +51,11 @@ const principal = () => {
 const thead = () => {
     let cab = document.createElement("thead")
     let linha = document.createElement("tr")
-    for (i = 0; i < dados[0].duracao; i++) {
+    for (i = 0; i <= dados[0].duracao + 1; i++) {
         let th = document.createElement("th")
-        if (i == 0) th.innerHTML = dados[0].projeto
-        else th.innerHTML = i
+        if (i == 0) th.innerHTML = "Tarefas"
+        else if (i == 1) th.innerHTML = "Recursos"
+        else th.innerHTML = i <= 10 ? "0" + (i - 1) : i - 1
         linha.appendChild(th)
     }
     return cab.appendChild(linha)
@@ -46,6 +67,7 @@ const tbody = () => {
         let linha = document.createElement("tr")
         let desc = document.createElement("td")
         let resp = document.createElement("td")
+        resp.setAttribute("style", "text-align: center;")
         desc.innerHTML = dados[i].descricao
         resp.innerHTML = dados[i].responsavel
         linha.appendChild(desc)
@@ -56,13 +78,16 @@ const tbody = () => {
     return corpo
 }
 
-const bts = (inicio, duracao, linha) => {
-    for (i = 2; i < dados[0].duracao; i++) {
+const bts = (inicio, duracao, l) => {
+    for (j = 1; j <= dados[0].duracao; j++) {
         let td = document.createElement("td")
-        td.innerHTML = '*'
-        linha.appendChild(td)
+        if (j >= inicio && j < inicio + duracao) {
+            let bt = document.createElement("button")
+            bt.setAttribute("class", "btn")
+            bt.setAttribute("onclick", "alert('Tarefa concluída')")
+            td.appendChild(bt)
+            td.setAttribute("style", "background-color: " + cores[dados[i].status])
+        }
+        l.appendChild(td)
     }
 }
-
-footer.innerHTML = "Rodapé"
-modal.innerHTML = "Modal"
