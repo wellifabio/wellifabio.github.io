32d336fb0d2a0550f87c63c9967b88489beb951f
wellifabio <wellifabio@hotmail.com>
2023-05-22 17:13:28 -0300

    pizzaria

--- pizzaria/cardapio/script.js
+++ pizzaria/cardapio/script.js
@@ -20,40 +20,47 @@ btFechar.addEventListener('click', () => {
 function preecherTabela() {
     dados.forEach((e, i) => {
         const linha = document.createElement("tr");
+        const img = document.createElement("td");
         const id = document.createElement("td");
         const nome = document.createElement("td");
         const preco = document.createElement("td");
         const detalhes = document.createElement("td");
+        img.innerHTML = `<img alt="logo" src=${e.img}>`
         id.innerHTML = e.id;
         nome.innerHTML = e.nome;
         preco.innerHTML = e.preco;
-        detalhes.innerHTML = `<button onclick="abrirModal('${i}')">Detalhes</button>`;
+        detalhes.innerHTML = `<button onclick="abrirModal('${i}')">Ver<br>detalhes</button>`;
+        linha.appendChild(img);
         linha.appendChild(id);
         linha.appendChild(nome);
         linha.appendChild(preco);
         linha.appendChild(detalhes);
         tcorpo.appendChild(linha)
     })
+    console.log(window.localStorage.getItem("itens"))
 }
 
 function abrirModal(i) {
     detalhes.classList.toggle("oculto");
     document.querySelector("#id").value = dados[i].id;
     document.querySelector("#nome").value = dados[i].nome;
+    document.querySelector("#img").src = dados[i].img;
+    document.querySelector("#descricao").value = dados[i].descricao;
+    document.querySelector("#nome").value = dados[i].nome;
 }
 
-function cadastarLocal() {
+function salvarLocalmente() {
     const item = {
         id: document.querySelector("#id").value,
         nome: document.querySelector("#nome").value
     }
 
-    //Abrir ou iniciar a lista de produtos
-    const produtos = JSON.parse(window.localStorage.getItem("produtos")) || []
+    //Abrir ou iniciar a lista de itens
+    const itens = JSON.parse(window.localStorage.getItem("itens")) || []
     //Acrescentar o novo item na lista
-    produtos.push(item)
+    itens.push(item)
     //Salvar a lista no armazenamento local
-    window.localStorage.setItem("produtos", JSON.stringify(produtos))
+    window.localStorage.setItem("itens", JSON.stringify(itens))
 
     //Recarregar a página
     window.location.reload()
