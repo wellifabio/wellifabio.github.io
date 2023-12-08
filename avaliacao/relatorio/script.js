27680544abdbdd570e293c044b835df699f99a84
wellifabio <wellifabio@hotmail.com>
2023-12-07 23:01:22 -0300

    avaliacao

--- avaliacao/relatorio/script.js
+++ avaliacao/relatorio/script.js
@@ -9,6 +9,8 @@ var avaliacao = {
     matriz: []
 }
 
+var notas = [], niveis = [], desempenho = [];
+
 const fileAvaliacao = document.getElementById("fileAvaliacao");
 const inputAvaliacao = document.getElementById("inputAvaliacao");
 const fileAlunos = document.getElementById("fileAlunos");
@@ -20,9 +22,6 @@ const local = JSON.parse(localStorage.getItem("avaliacao"));
 if (local) {
     avaliacao = local;
     montarAvaliacao();
-    console.table(avaliacao.alunos);
-    console.table(avaliacao.criterios);
-    console.table(avaliacao.matriz);
 }
 
 function reiniciar() {
@@ -118,30 +117,82 @@ function alunosMatriz() {
     avaliacao.alunos.forEach(aluno => {
         let td = document.createElement("td");
         td.classList.add("tv2");
+        td.setAttribute("contentEditable", "true");
         td.innerHTML = aluno.aluno;
         alunos.appendChild(td);
     });
 }
 
-function criteriosMatriz() {
-    const criTecnicos = document.getElementById("criteriosTecnicos");
-    const criGestao = document.getElementById("criteriosGestao");
+function criteriosMatriz(tg) {
+    const tbody = document.getElementById("tbody");
+    let nT = 0;
+    let mesclar = 0;
+    let competencias = "Competências de Gestão";
     avaliacao.criterios.forEach(cri => {
+        if (cri.tg == "T") nT++;
+    });
+    let nG = avaliacao.criterios.length - nT;
+    if (tg == "T") { mesclar = nT; competencias = "Competências Técnicas"; } else mesclar = nG;
+    let primeiraLinha = true;
+    avaliacao.criterios.forEach((cri, i) => {
         let tr = document.createElement("tr");
-        let fund = document.createElement("td");
-        let crit = document.createElement("td");
-        if(cri.criticidade == 1) crit.classList.add("critico");
-        else crit.classList.add("desejavel");
-        fund.innerHTML = cri.fundamento;
-        crit.innerHTML = cri.criterio;
-        tr.appendChild(fund);
-        tr.appendChild(crit);
-        if (cri.tg == "T") {
-            criTecnicos.appendChild(tr);
-        } else {
-            criGestao.appendChild(tr);
+        let fundamento = document.createElement("td");
+        let criterio = document.createElement("td");
+        let notas = document.createElement("td");
+        fundamento.classList.add("fundamentos");
+        if (cri.criticidade == 1) criterio.classList.add("critico");
+        else criterio.classList.add("desejavel");
+        fundamento.setAttribute("contentEditable", "true");
+        criterio.setAttribute("contentEditable", "true");
+        fundamento.innerHTML = cri.fundamento;
+        criterio.innerHTML = cri.criterio;
+        if (cri.tg == tg) {
+            if (primeiraLinha) {
+                let lateral = document.createElement("th");
+                lateral.classList.add("tv");
+                lateral.setAttribute("rowspan", mesclar);
+                lateral.innerHTML = competencias;
+                tr.appendChild(lateral);
+                primeiraLinha = false;
+            }
+            tr.appendChild(fundamento);
+            tr.appendChild(criterio);
+            let tabNotas = document.createElement("table");
+            tabNotas.classList.add("tabNotas");
+            let tabBody = document.createElement("tbody");
+            let tabTr = document.createElement("tr");
+            tabBody.appendChild(tabTr);
+            tabNotas.appendChild(tabBody);
+            avaliacao.matriz[i].forEach((linha, j) => {
+                let td = document.createElement("td");
+                td.setAttribute("style", "height:100%;border-bottom:none;margin:0;padding:0;");
+                let inp = document.createElement("input");
+                inp.id = i + "_" + j;
+                inp.setAttribute("onclick", "nota(this.id)");
+                inp.classList.add("nota");
+                inp.type = "button";
+                inp.value = linha;
+                td.appendChild(inp);
+                tabTr.appendChild(td);
+            });
+            notas.setAttribute("style", "margin:0;padding:0;");
+            notas.appendChild(tabNotas);
+            tr.appendChild(notas);
+            tbody.appendChild(tr);
         }
     });
+}
+
+function nota(id) {
+    let i = parseInt(id.split("_")[0]);
+    let j = parseInt(id.split("_")[1]);
+    let nota = document.getElementById(id).value;
+    if (nota == 0) nota = 1; else nota = 0;
+    avaliacao.matriz[i][j] = nota;
+    localStorage.setItem("avaliacao", JSON.stringify(avaliacao));
+    document.getElementById("tbody").innerHTML = "";
+    criteriosMatriz("T");
+    criteriosMatriz("G");
 }
 
 function montarAvaliacao() {
@@ -162,6 +213,34 @@ function montarAvaliacao() {
         document.getElementById("inputCriterios").classList.remove("oculto");
     else {
         alunosMatriz();
-        criteriosMatriz();
+        criteriosMatriz("T");
+        criteriosMatriz("G");
+        calculaDesempenho();
     }
+}
+
+function calculaDesempenho() {
+    let nCriticos = 0;
+    let nDesejaveis = 0;
+    avaliacao.criterios.forEach(cri => {
+        if (cri.criticidade != '1') nDesejaveis++;
+        else nCriticos++;
+    });
+    let decMais50 = Math.floor(50 / nDesejaveis);
+    let decMenos50 = Math.floor(50 / nCriticos);
+    let not = 100;
+    desempenho.push({ "descricao": "Atingiu todos os critérios críticos e desejáveis", "nivel": 1, "nota": not });
+    for (let i = 1; i < nDesejaveis; i++) {
+        not -= decMais50;
+        desempenho.push({ "descricao": `Atingiu todos os critérios críticos e ${nDesejaveis - i} desejáveis`, "nivel": i + 1, "nota": not });
+    }
+    not=50;
+    desempenho.push({ "descricao": "Atingiu todos os critérios críticos", "nivel": nDesejaveis + 1, "nota": not });
+    for (let i = 1; i < nCriticos; i++) {
+        not -= decMenos50;
+        desempenho.push({ "descricao": `Atingiu ${nCriticos - i} critérios críticos`, "nivel": nDesejaveis + i + 1, "nota": not });
+    }
+    console.table(desempenho);
+    console.log(nCriticos);
+    console.log(nDesejaveis);
 }
