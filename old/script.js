9b40e792d8b9dba2c31994b1d4a9326bd467e081
wellifabio <wellifabio@hotmail.com>
2024-09-09 14:55:33 -0300

    novo

---
+++ old/script.js
@@ -0,0 +1,282 @@
+const aulas = [
+	{
+		href: "./canvas",
+		target: "_blank",
+		text: "JS: Canvas Frame Maker (pt-br)"
+	},
+	{
+		href: "./gantt",
+		target: "_blank",
+		text: "JS: Gantt chart generator (pt-br)"
+	},
+	{
+		href: "./avaliacao",
+		target: "_blank",
+		text: "JS: SENAI standard assessment generator (pt-br)"
+	},
+	{
+		href: "./timeline",
+		target: "_blank",
+		text: "JS: Gantt chart generator"
+	},
+	{
+		href: "./bitola",
+		target: "_blank",
+		text: "JS: Cálc. Bitola p/ Cabo de Cobre (pt-br)"
+	},
+	{
+		href: "./siminvest",
+		target: "_blank",
+		text: "JS: Simulador de Investimentos (pt-br)"
+	},
+	{
+		href: "./gadgets",
+		target: "_blank",
+		text: "JS - Gadgets"
+	},
+	{
+		href: "./pizzaria/carrinho",
+		target: "_blank",
+		text: "JS - Pizzaria Modelo (pt-br)"
+	},
+	{
+		href: "./delivery/delivery.html",
+		target: "_blank",
+		text: "JS - Delivery Manager (pt-br)"
+	},
+	{
+		href: "./memoria",
+		target: "_blank",
+		text: "JS - Memory Game (pt - br)"
+	},
+	{
+		href: "./clima",
+		target: "_blank",
+		text: "JS - Learning ChartJS"
+	},
+	{
+		href: "./sena/bilhete.html",
+		target: "_blank",
+		text: "Brazil lottery game generator Sena (pt - br)"
+	},
+	{
+		href: "./loto/bilhete.html",
+		target: "_blank",
+		text: "Brazil lottery game generator Loto<br (pt - br)"
+	},
+	{
+		href: "./hash",
+		target: "_blank",
+		text: "JS - Hash Game"
+	},
+	{
+		href: "./chat",
+		target: "_blank",
+		text: "Chatroom / Sala de Bate papo (pt - br)"
+	},
+	{
+		href: "./rand",
+		target: "_blank",
+		text: "Randomizer data/Gerador de dados aleatórios (pt - br)"
+	},
+	{
+		href: "./petshop",
+		target: "_blank",
+		text: "PetShop App Basic/PetShop básico Arquivos Locais (pt - br)"
+	},
+	{
+		href: "https://receitaswell.azurewebsites.net/",
+		target: "_blank",
+		text: "Cookbook blog (Blog de Receitas) (pt - br)"
+	},
+	{
+		href: "https://github.com/wellifabio/senai2022/blob/master/old/1des/fpoo/aula25/PetsLogin/doc/prototipo.pdf",
+		target: "_blank",
+		text: "Java Desktop - PetShop Solution example<br (pt - br)"
+	},
+	{
+		href: "https://github.com/wellifabio/senai2021/blob/master/1des/fpoo/exemplosGUI/ProjetoHortelino/doc/prototipo.pdf",
+		target: "_blank",
+		text: "Java Desktop - Inventory and sales management<br (pt - br)"
+	},
+	{
+		href: "https://github.com/wellifabio/senai2021/blob/master/1des/fpoo/exemplosGUI/Estacionamento/docs/prototipo.pdf",
+		target: "_blank",
+		text: "Java Desktop - Park Solution example<br (pt - br)"
+	},
+	{
+		href: "https://github.com/wellifabio/senai2021/blob/master/1des/fpoo/exemplosGUI/Mapeador/docs/prototipo.pdf",
+		target: "_blank",
+		text: "Java Desktop - Small Nets Solution example<br (pt - br)"
+	},
+	{
+		href: "https://github.com/wellifabio/senai2021/blob/master/1des/fpoo/exemplosGUI/Money/docs/prototipo.pdf",
+		target: "_blank",
+		text: "Java Desktop - Counting money Solution example<br (pt - br)"
+	}
+];
+
+const projetos = [
+	{
+		href: "./assets/containner/",
+		target: "_blank",
+		text: "Projects AutoCad 3D - Container Snack Bar"
+	},
+	{
+		href: "./assets/diversos/",
+		target: "_blank",
+		text: "Projects AutoCad 3D modelling"
+	},
+	{
+		href: "./assets/academia/",
+		target: "_blank",
+		text: "My architect CAD hobby"
+	},
+	{
+		href: "./assets/minhacasa/",
+		target: "_blank",
+		text: "My building hobby"
+	},
+	{
+		href: "./assets/diversos/miniold.html",
+		target: "_blank",
+		text: "Web Site - Old<br (pt - br)"
+	},
+]
+
+const ports = [
+	{
+		"Nome":"Matheus Pignata",
+		"link":"https://matheuspignata.github.io"
+	},
+	{
+		"Nome": "Andre Fernandes Izidro",
+		"link": "https://aizidro.github.io"
+	},
+	{
+		"Nome": "Lucas Almeida Camacho",
+		"link": "https://lcscamacho.github.io"
+	},
+	{
+		"Nome": "Vinicius Vieira de Souza",
+		"link": "https://vinisouza001.github.io/"
+	},
+	{
+		"Nome": "Yukina Madum Shinhama",
+		"link": "https://portfolio-2023-delta-sable.vercel.app/"
+	},
+	{
+		"Nome": "Ana Clara Ortiz Ochner",
+		"link": "https://anyocher.github.io"
+	},
+	{
+		"Nome": "Bruno Otávio da Silva Ramos",
+		"link": "https://bruno-otavio.github.io"
+	},
+	{
+		"Nome": "Carla Carota Mozena",
+		"link": "https://carla-coder.github.io"
+	},
+	{
+		"Nome": "Carlos Augusto Rodrigues",
+		"link": "https://carlosaugustorodrigues.github.io"
+	},
+	{
+		"Nome": "Carlos Henrique de Oliveira Siqueira",
+		"link": "https://carloshosiqueira.github.io"
+	},
+	{
+		"Nome": "Cicero Ruan Soares Barbosa ",
+		"link": "https://portfoliocisenai.netlify.app/"
+	},
+	{
+		"Nome": "Gabriella Godoi Avila",
+		"link": "https://avilagabriella.github.io"
+	},
+	{
+		"Nome": "Guilherme Carvalho De Oliveira",
+		"link": "https://caroliflow.000webhostapp.com"
+	},
+	{
+		"Nome": "Igor Henrique GonÃ§alves",
+		"link": "https://igorhenriqueg.github.io"
+	},
+	{
+		"Nome": "Jaqueline Rodrigues",
+		"link": "https://JaqueJak.github.io"
+	},
+	{
+		"Nome": "João Victor Colosso",
+		"link": "https://portfoliocolosso.netlify.app"
+	},
+	{
+		"Nome": "João Vitor Castro Viana Marques",
+		"link": "https://JMarques12.github.io"
+	},
+	{
+		"Nome": "Leticia Aparecida Hofman de Souza",
+		"link": "https://lehhofman.github.io"
+	},
+	{
+		"Nome": "Otavio Bassi de freitas",
+		"link": "https://freitas7portfolio.netlify.app"
+	},
+	{
+		"Nome": "Vinícius Manzano dos Santos",
+		"link": "https://vinimanzano.github.io/vinimanzano.io"
+	},
+	{
+		"Nome": "Alefe de Magalhães Vigatto",
+		"link": "https://alefedem.000webhostapp.com/"
+	},
+	{
+		"Nome": "Camilla Piva Da Silva",
+		"link": "https://Camispiva.github.io"
+	},
+	{
+		"Nome": "Maria Fernanda Mattoso Oliveira Rello",
+		"link": "https://fefemattoso.github.io"
+	},
+	{
+		"Nome": "Erick da Silva de Jesus",
+		"link": "https://ericksilva232323.github.io/"
+	},
+	{
+		"Nome": "Richard Matheus Pinesi",
+		"link": "https://richardpnsportfolio.netlify.app"
+	},
+	{
+		"Nome": "Otavio Bueno",
+		"link": "https://buenootavio.github.io/"
+	},
+		{
+		"Nome": "Bryan Beckham",
+		"link": "https://bryanbeckham06.github.io/"
+	}
+];
+
+const als = document.querySelector("#aulas");
+const prjts = document.querySelector("#projetos");
+const portfolios = document.querySelector("#portLink");
+
+const load = () => {
+	aulas.forEach(aula => {
+		als.innerHTML += `<a href="${aula.href}" target="${aula.target}" class="postit">${aula.text}</a>`;
+	});
+	projetos.forEach(projeto => {
+		prjts.innerHTML += `<a href="${projeto.href}" target="${projeto.target}" class="postitred">${projeto.text}</a>`;
+	});
+	ports.forEach(pt => {
+		portfolios.innerHTML += `<a href="${pt.link}" target="_blank" class="postitgreen">${pt.Nome}</a>`;
+	});
+}
+
+//Função para calcular idade
+function calcularIdade(nascimento) {
+	var nascimento = new Date(nascimento);
+	var diferenca = Date.now() - nascimento.getTime();
+	var idade = new Date(diferenca);
+	return Math.abs(idade.getUTCFullYear() - 1970);
+}
+
+document.querySelector("#idade").innerHTML = calcularIdade("1980/09/08");
