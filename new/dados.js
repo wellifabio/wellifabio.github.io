a28ce4143aadc3a413f0a38e40ac38b171374789
wellifabio <wellifabio@hotmail.com>
2024-09-16 08:26:53 -0300

    markdown

--- new/dados.js
+++ new/dados.js
@@ -71,6 +71,10 @@ const aplicacoes = [
 		href: "../petshop",
 		text: "PetShop App Basic/PetShop básico Arquivos Locais (pt - br)"
 	}
+	{
+		href: "https://github.com/wellifabio/matematica.git",
+		text: "Mathematical Expressions with Markdown (pt - br)"
+	}
 ];
 
 const portfolios = [
