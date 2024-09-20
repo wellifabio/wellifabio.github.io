d5940cd3ed8980137f336f984acfd9d909a5c5a5
wellifabio <wellifabio@hotmail.com>
2024-09-20 16:44:46 -0300

    receitas

--- cookbook/admin.js
+++ cookbook/admin.js
@@ -2,6 +2,7 @@
 const uri = "https://cookwellifabio.vercel.app/"
 const user = JSON.parse(localStorage.getItem("user"))
 const cards = document.querySelector("#cards")
+const cadastro = document.querySelector("#cadastro")
 
 if (!user) {
     window.location.href = "./index.html"
@@ -16,14 +17,19 @@ const iniciar = () => {
                 let foto = './assets/default.png'
                 if (receita.foto) foto = 'data:image/png;base64,' + receita.foto
                 card.innerHTML += `
-                <div class="card ${receita.tipo}" id="${receita._id}">
+                <div class="card ${receita.tipo}">
                     <div class="card_header">
                         <div>
-                            <b>Id:</b><label>${receita._id}</label><button class="btUpDel" onclick="excluir(${receita._id})">-</button>
+                            <b>Id:</b>
+                            <label>${receita._id}</label>
+                            <div style="max-width:120px">
+                                <button class="btUpDel" onclick="update('${receita._id}')">*</button>
+                                <button class="btUpDel" onclick="excluir('${receita._id}')">-</button>
+                            </div>
                         </div>
                     </div>
                     <div>
-                        <form>
+                        <form id="${receita._id}">
                             <div>
                                 <b>Nome:</b>
                                 <input type="text" name="nome" value="${receita.nome}"/>
@@ -46,10 +52,9 @@ const iniciar = () => {
                             <div>
                                 <label>Imagem:</label>
                                 <input type="file" onchange="prevImg('img${receita._id}',this)" accept="image/jpeg, image/png")>
-                                <input type="hidden"  name="foto" value=${receita.foto} id="fileimg${receita._id}">
+                                <input type="hidden"  name="foto" value="${receita.foto}" id="fileimg${receita._id}">
                             </div>
                             <img class="img" src="${foto}" id="img${receita._id}")>
-                            <button class="btUpDel" type="submit">Atualizar</button>
                         </form>
                     </div>
                 </div>`
@@ -63,5 +68,71 @@ function logout() {
     window.location.href = "./index.html"
 }
 
+cadastro.addEventListener("submit", e => {
+    e.preventDefault()
+    const nome = cadastro.nome.value
+    const tipo = cadastro.tipo.value
+    const ingredientes = cadastro.ingredientes.value
+    const modoPreparo = cadastro.modoPreparo.value
+    const foto = cadastro.foto.value
 
+    fetch(uri + "receita", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ nome, tipo, ingredientes, modoPreparo, foto })
+    })
+        .then(data => data.status)
+        .then(data => {
+            if (data == 201) {
+                window.location.reload()
+            } else {
+                alert("Erro ao registrar receita")
+            }
+        })
+})
 
+function update(id) {
+    const alterar = document.getElementById(id)
+    const dados = {
+        nome: alterar.nome.value,
+        tipo: alterar.tipo.value,
+        ingredientes: alterar.ingredientes.value,
+        modoPreparo: alterar.modoPreparo.value,
+        foto: alterar.foto.value
+    }
+    fetch(uri + "receita/" + alterar.id, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dados)
+    })
+        .then(data => data.status)
+        .then(data => {
+            if (data == 202) {
+                window.location.reload()
+            } else {
+                alert("Erro ao alterar receita")
+            }
+        })
+}
+
+function excluir(id) {
+    if (confirm("Confirma a exclusão desta receita?"))
+        fetch(uri + "receita/" + id, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(data => data.status)
+            .then(data => {
+                if (data == 204) {
+                    window.location.reload()
+                } else {
+                    alert("Erro ao excluir receita")
+                }
+            })
+}
